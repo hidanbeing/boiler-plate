547558910bfa18ce07d497c86de2d6e63e4344fb
이단비 <medanbee@gmail.com>
2023-01-06 01:34:09 +0900

    front-backend 연결 1

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,17 +1,35 @@
 import React, {useEffect} from 'react'
 import axios from "axios"
-
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
+
+  const navigate = useNavigate();
 
   useEffect(()=>{
     axios.get('/api/hello')
     .then(res => console.log(res.data))
   },[])
 
+  const onClickHandler = () => {
+    axios.get('/api/users/logout')
+    .then(res => {
+      if (res.data.success){
+        navigate('/login'); 
+      }
+      else{
+        alert("로그아웃 하는 데 실패했습니다.");
+      }
+     
+    })
+  }
+
   return (
-    <div>
-      LandingPage
+    <div style={{
+      display: 'flex', justifyContent: 'center', alignItems: 'center',
+      width:'100%', height: '100vh'
+    }}>
+      <button onClick = {onClickHandler}>로그아웃</button>
     </div>
   )
 }
